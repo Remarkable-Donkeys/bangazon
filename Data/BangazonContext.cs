b5a24b43b8c3bb588c304d281468cf3935f87552
Tyler <bowmant388@gmail.com>
2018-01-24 10:50:38 -0600

    initial commit

---
+++ Data/BangazonContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Models;
+
+namespace Bangazon.Data
+{
+    public class BangazonContext : DbContext
+    {
+        public BangazonContext(DbContextOptions<BangazonContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Customer> Customer { get; set; }
+        // public DbSet<ProductType> ProductyType { get; set; }
+        // public DbSet<PaymentType> PaymentType { get; set; }
+        // public DbSet<Products> Products { get; set; }
+        // public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        // public DbSet<Departments> Departments { get; set; }
+        // public DbSet<TrainingPrograms> TrainingPrograms { get; set; }
+        // public DbSet<Computer> Computer { get; set; }
+        // public DbSet<Employees> Employees { get; set; }
+        // public DbSet<EmployeeComputer> EmployeeComputer { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Entity<Order>()
+            //     .Property(b => b.DateCreated)
+            //     .HasDefaultValueSql("strftime('%Y-%m-%d %H:%M:%S')");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bangazon.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(55)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(55)]
+        public string LastName { get; set; }
+
+        public DateTime LastActive { get; set; }   
+    }
+}
