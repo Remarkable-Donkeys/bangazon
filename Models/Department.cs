80c08a9c7eb922ebbb2b522deebbe2a672e916ed
Sean Williams <dacamor85@gmail.com>
2018-01-24 15:29:55 -0600

    minor changes to help remove red squiggles

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,4 +1,9 @@
-namespace bangazon.Models
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bangazon.Models
 {
     public class Department
     {
--- Models/EmployeeComputer.cs
+++ Models/EmployeeComputer.cs
@@ -1,7 +1,13 @@
-namespace bangazon.Models
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bangazon.Models
 {
     public class EmployeeComputer
     {
-        
+        [Key]
+        public int EmployeeComputerId   {get; set;}
     }
 }
