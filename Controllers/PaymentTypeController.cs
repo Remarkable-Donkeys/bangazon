1aa1c65c9389d9a46d886a9d81694e7553f1995d
Kristen Norris <krnorris65@gmail.com>
2018-01-24 11:53:57 -0600

    created a blank Training Program model and started setup for the Payment Type controller

--- Controllers/PaymentTypeController.cs
+++ Controllers/PaymentTypeController.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Data;
+using Bangazon.Models;
+
 namespace Bangazon.Controllers
 {
-    public class PaymentTypeController
+    [Route("api/[controller]")]
+    public class PaymentTypeController : Controller
     {
+        private BangazonContext _context;
+        // Constructor method to create an instance of context to communicate with our database.
         
     }
 }
